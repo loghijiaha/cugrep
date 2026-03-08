a3065c159563aa052400b9c363effcf42c8c129a
Loghi Perinpanayagam <lper1582@sysco.com>
2026-03-09 03:43:56 +0800

    Update cli.py

--- python/pycugrep/cli.py
+++ python/pycugrep/cli.py
@@ -4,7 +4,6 @@ import os
 import sys
 from importlib.resources import files as importlib_files
 
-
 def main():
     parser = argparse.ArgumentParser(
         description="A GPU-accelerated grep tool."
